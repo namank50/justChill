4c26e41e0006af52f5f0227c4a18759f3282c7fd
namank50 <namank50@gmail.com>
2021-05-14 18:28:34 +0530

    firsti

---
+++ formValidation1234/formValidation1234/src/main/java/com/example/formValidation/formValidation1234/FormValidation1234Application.java
@@ -0,0 +1,13 @@
+package com.example.formValidation.formValidation1234;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FormValidation1234Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FormValidation1234Application.class, args);
+	}
+
+}
---
+++ formValidation1234/formValidation1234/src/test/java/com/example/formValidation/formValidation1234/FormValidation1234ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.formValidation.formValidation1234;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FormValidation1234ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
